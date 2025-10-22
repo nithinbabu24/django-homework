3117c6418ede54db07fdfe447aa13450261cb0a0
nithinbabu24 <Nithinbabau020@yahoo.com>
2025-10-22 14:06:02 +0530

    changes

--- django_HW_app/views.py
+++ django_HW_app/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 
 def login_view(request):
+    error_full_name = ""
     error_email = ""
     error_password = ""
     message = ""
 
     if request.method == "POST":
+        full_name = request.POST.get("full_name")
         email = request.POST.get("email")
         password = request.POST.get("password")
+
+        
+        if not full_name:
+            error_full_name = "Full name is required."
+        elif len(full_name) < 2:
+            error_full_name = "Full name must be at least 2 characters long."
 
         
         if not email:
@@ -23,11 +31,12 @@ def login_view(request):
         elif len(password) < 6:
             error_password = "Password must be at least 6 characters long."
 
-    
-        if not error_email and not error_password:
-            message = f"Thanks for registering, {email}!"
+        
+        if not error_full_name and not error_email and not error_password:
+            message = f"Thanks for registering, {full_name}!"
 
     return render(request, "login.html", {
+        "error_full_name": error_full_name,
         "error_email": error_email,
         "error_password": error_password,
         "message": message
